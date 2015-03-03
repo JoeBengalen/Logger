f950ac546aed1e9ba434a9e538a97455e28b70b9
JoeBengalen <JoeBengalen@users.noreply.github.io>
2015-03-03 19:59:42 +0100

    Added an autoloader for test files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,11 @@
 <?php
 
 require dirname(__DIR__) . '/vendor/autoload.php';
+
+// loader for some extra file used by soem tests
+spl_autoload_register(function ($class) {
+    $file = rtrim(dirname(__FILE__), '/') . '/' . $class . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
