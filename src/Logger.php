252db360c088931125f317b9cdff9f04a97fc1dd
JoeBengalen <joebengalen@gmail.com>
2015-03-05 17:25:22 +0100

    Updated the file level docblocks

--- src/Logger.php
+++ src/Logger.php
@@ -1,31 +1,11 @@
 <?php
 /**
- * Logger - Lightweight psr3 logger library
+ * JoeBengalen Logger library
  * 
- * @author      Martijn Wennink
- * @copyright   2015 Martijn Wennink
+ * @author      Martijn Wennink <joebengalen@gmail.com>
+ * @copyright   Copyright (c) 2015 Martijn Wennink
+ * @license     https://github.com/JoeBengalen/Logger/blob/master/LICENSE.md (MIT License)
  * @version     0.1.0
- * @package     JoeBengalen\Logger
- *
- * MIT LICENSE
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
  */
 namespace JoeBengalen\Logger;
 
@@ -40,9 +20,6 @@ use Psr\Log\InvalidArgumentException;
  * Lightweight logger class thats implements the psr3 interface. Handlers should 
  * be registered to this logger. The handlers must do the actual logging task. 
  * This class just passes the log details to each registered handler.
- * 
- * @package Logger
- * @author  Martijn Wennink
  */
 class Logger implements LoggerInterface
 {
